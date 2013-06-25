647ec04996a4facbc886568ee7e81cb9518436ca
Caio Marcelo de Oliveira Filho <caio.de.oliveira.filho@intel.com>
2013-06-25 19:19:33 -0300

    extensions: tool for generating C source from JS file

---
+++ src/extensions/tools/generate_api.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath("../v8/tools/js2c.py")))
+
+import js2c
+
+TEMPLATE = """\
+static const char kGeneratedSource[] = { %s, 0 };
+"""
+
+js_code = sys.argv[1]
+lines = js2c.ReadFile(js_code)
+c_code = js2c.ToCAsciiArray(lines);
+
+output = open(sys.argv[2], "w")
+output.write(TEMPLATE % c_code);
+output.close()
